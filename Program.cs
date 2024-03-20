ee7cf4859601522ca9dd19d996e39b27dc365254
Naeliza <lizcespedes0326@gmail.com>
2024-03-20 07:46:19 -0400

    Instalación del Proveedor de Base de Datos MySQL

--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Proyecto_Final_Software_Seguro.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configurar la conexión a la base de datos MySQL
+builder.Services.AddDbContext<TaskDbContext>((serviceProvider, options) =>
+{
+    var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+    var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
+
+var app = builder.Build();
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
