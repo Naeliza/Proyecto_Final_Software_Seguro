2f7719862ca61db6c50fbe0381867ed9c905acc4
Naeliza <lizcespedes0326@gmail.com>
2024-03-22 18:10:50 -0400

    Añadiendo migracion

--- Migrations/20240322220839_Psswordhash.Designer.cs
+++ Migrations/20240322220839_Psswordhash.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Proyecto_Final_Software_Seguro.Models;
 
@@ -10,9 +11,10 @@ using Proyecto_Final_Software_Seguro.Models;
 namespace Proyecto_Final_Software_Seguro.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240322220839_Psswordhash")]
+    partial class Psswordhash
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
