5b6fcd946bc58bf1a5a15227fd3b409fd267af08
Naeliza <lizcespedes0326@gmail.com>
2024-03-21 22:00:39 -0400

    Controlador de Tareas

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proyecto_Final_Software_Seguro.Models;
+using System.Linq;
+
+namespace Proyecto_Final_Software_Seguro.Controllers
+{
+    public class TasksController : Controller
+    {
+        private readonly TaskDbContext _context;
+
+        public TasksController(TaskDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tasks/Index
+        public IActionResult Index()
+        {
+            var tasks = _context.Tasks.ToList();
+            return View(tasks);
+        }
+    }
+}
