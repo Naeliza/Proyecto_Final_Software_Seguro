3d2bee5a9972fd557868da051e9170537afba96c
Naeliza <lizcespedes0326@gmail.com>
2024-03-22 20:45:11 -0400

    Haciendo modelos para pantalla de registro

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Proyecto_Final_Software_Seguro.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; } // Propiedad para almacenar la contraseña en texto plano
+        public string PasswordHash { get; set; } // Propiedad para almacenar el hash de la contraseña
+    }
+}
