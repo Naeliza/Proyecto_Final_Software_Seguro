600255e9ae3384e0bc224007ba7e9a801b2aa0b6
Naeliza <lizcespedes0326@gmail.com>
2024-03-22 20:45:50 -0400

    Añadiendo pantalla de registro

---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_Final_Software_Seguro.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "El nombre de usuario es requerido.")]
+        [Display(Name = "Nombre de Usuario")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es requerida.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar Contraseña")]
+        [Compare("Password", ErrorMessage = "Las contraseñas no coinciden.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
