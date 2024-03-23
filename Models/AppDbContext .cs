5ef581e226ebfdd88fd9901adaa5ac5c8a0cb6f2
Naeliza <lizcespedes0326@gmail.com>
2024-03-22 20:44:32 -0400

    Modificando AppDbContext

--- Models/AppDbContext .cs
+++ Models/AppDbContext .cs
@@ -1,15 +1,13 @@
-﻿
-    using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
-    namespace Proyecto_Final_Software_Seguro.Models
+namespace Proyecto_Final_Software_Seguro.Models
+{
+    public class AppDbContext : DbContext
     {
-        public class AppDbContext : DbContext
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-            public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-            {
-            }
-
-            public DbSet<TaskItem> Tasks { get; set; }
-            public DbSet<LoginViewModel> Users { get; set; }
         }
+        public DbSet<TaskItem> Tasks { get; set; }
+        public DbSet<User> Users { get; set; } // Cambiar LoginViewModel a User
     }
+}
